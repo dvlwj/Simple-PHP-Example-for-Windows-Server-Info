adb128d895166cb2b0f0b9dfe0e89ad78ac67dc6
David <30721971+dvlwj@users.noreply.github.com>
2020-08-30 20:43:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php
 require_once('SystemInfoRAM.php');
 require_once('SystemInfoCPU.php');
-$server_local_datetime		= date("Y-m-d h:i:sa");
+$server_local_datetime		= date("l, d F Y h:i:s A");
+date_default_timezone_set("Asia/Tokyo");
+$server_japan_datetime		= date("l, d F Y h:i:s A");
 date_default_timezone_set("Asia/Jakarta");
-$server_gmt7_datetime		= date("Y-m-d h:i:sa");
+$server_gmt7_datetime		= date("l, d F Y h:i:s A");
 $ram = getMemoryServer();
 $cpu = getProcessorServer();
 $data = [
-	'message' => 'Hello, below is information for the server:',
-	'date_time' => $server_local_datetime,
+	'message' => "Hello, Your server status per {$server_local_datetime} is",
+	'date_time' => $server_japan_datetime,
 	'date_time_gmt7' => $server_gmt7_datetime,
 	'memory_usage' => $ram,
 	'cpu_usage' => $cpu,
