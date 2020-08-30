054a6eb386589c461edf6f94811e5d21b931e0b3
David <30721971+dvlwj@users.noreply.github.com>
2020-08-30 20:44:44 +0700

    Update SystemInfoRAM.php

--- SystemInfoRAM.php
+++ SystemInfoRAM.php
@@ -1,34 +1,65 @@
 <?php
 
-    // Returns used memory (either in percent (without percent sign) or free and overall in bytes)
-    function getServerMemoryUsage($getPercentage=true)
+    header("Content-Type: text/plain");
+
+    function _getServerLoadLinuxData()
     {
-        $memoryTotal = null;
-        $memoryFree = null;
+        if (is_readable("/proc/stat"))
+        {
+            $stats = @file_get_contents("/proc/stat");
 
-        if (stristr(PHP_OS, "win")) {
-            // Get total physical memory (this is in bytes)
-            $cmd = "wmic ComputerSystem get TotalPhysicalMemory";
-            @exec($cmd, $outputTotalPhysicalMemory);
+            if ($stats !== false)
+            {
+                // Remove double spaces to make it easier to extract values with explode()
+                $stats = preg_replace("/[[:blank:]]+/", " ", $stats);
 
-            // Get free physical memory (this is in kibibytes!)
-            $cmd = "wmic OS get FreePhysicalMemory";
-            @exec($cmd, $outputFreePhysicalMemory);
+                // Separate lines
+                $stats = str_replace(array("\r\n", "\n\r", "\r"), "\n", $stats);
+                $stats = explode("\n", $stats);
 
-            if ($outputTotalPhysicalMemory && $outputFreePhysicalMemory) {
-                // Find total value
-                foreach ($outputTotalPhysicalMemory as $line) {
-                    if ($line && preg_match("/^[0-9]+\$/", $line)) {
-                        $memoryTotal = $line;
-                        break;
+                // Separate values and find line for main CPU load
+                foreach ($stats as $statLine)
+                {
+                    $statLineData = explode(" ", trim($statLine));
+
+                    // Found!
+                    if
+                    (
+                        (count($statLineData) >= 5) &&
+                        ($statLineData[0] == "cpu")
+                    )
+                    {
+                        return array(
+                            $statLineData[1],
+                            $statLineData[2],
+                            $statLineData[3],
+                            $statLineData[4],
+                        );
                     }
                 }
+            }
+        }
 
-                // Find free value
-                foreach ($outputFreePhysicalMemory as $line) {
-                    if ($line && preg_match("/^[0-9]+\$/", $line)) {
-                        $memoryFree = $line;
-                        $memoryFree *= 1024;  // convert from kibibytes to bytes
+        return null;
+    }
+
+    // Returns server load in percent (just number, without percent sign)
+    function getServerLoad()
+    {
+        $load = null;
+
+        if (stristr(PHP_OS, "win"))
+        {
+            $cmd = "wmic cpu get loadpercentage /all";
+            @exec($cmd, $output);
+
+            if ($output)
+            {
+                foreach ($output as $line)
+                {
+                    if ($line && preg_match("/^[0-9]+\$/", $line))
+                    {
+                        $load = $line;
                         break;
                     }
                 }
@@ -36,77 +67,49 @@
         }
         else
         {
-            if (is_readable("/proc/meminfo"))
+            if (is_readable("/proc/stat"))
             {
-                $stats = @file_get_contents("/proc/meminfo");
+                // Collect 2 samples - each with 1 second period
+                // See: https://de.wikipedia.org/wiki/Load#Der_Load_Average_auf_Unix-Systemen
+                $statData1 = _getServerLoadLinuxData();
+                sleep(1);
+                $statData2 = _getServerLoadLinuxData();
 
-                if ($stats !== false) {
-                    // Separate lines
-                    $stats = str_replace(array("\r\n", "\n\r", "\r"), "\n", $stats);
-                    $stats = explode("\n", $stats);
+                if
+                (
+                    (!is_null($statData1)) &&
+                    (!is_null($statData2))
+                )
+                {
+                    // Get difference
+                    $statData2[0] -= $statData1[0];
+                    $statData2[1] -= $statData1[1];
+                    $statData2[2] -= $statData1[2];
+                    $statData2[3] -= $statData1[3];
 
-                    // Separate values and find correct lines for total and free mem
-                    foreach ($stats as $statLine) {
-                        $statLineData = explode(":", trim($statLine));
+                    // Sum up the 4 values for User, Nice, System and Idle and calculate
+                    // the percentage of idle time (which is part of the 4 values!)
+                    $cpuTime = $statData2[0] + $statData2[1] + $statData2[2] + $statData2[3];
 
-                        //
-                        // Extract size (TODO: It seems that (at least) the two values for total and free memory have the unit "kB" always. Is this correct?
-                        //
-
-                        // Total memory
-                        if (count($statLineData) == 2 && trim($statLineData[0]) == "MemTotal") {
-                            $memoryTotal = trim($statLineData[1]);
-                            $memoryTotal = explode(" ", $memoryTotal);
-                            $memoryTotal = $memoryTotal[0];
-                            $memoryTotal *= 1024;  // convert from kibibytes to bytes
-                        }
-
-                        // Free memory
-                        if (count($statLineData) == 2 && trim($statLineData[0]) == "MemFree") {
-                            $memoryFree = trim($statLineData[1]);
-                            $memoryFree = explode(" ", $memoryFree);
-                            $memoryFree = $memoryFree[0];
-                            $memoryFree *= 1024;  // convert from kibibytes to bytes
-                        }
-                    }
+                    // Invert percentage to get CPU time, not idle time
+                    $load = 100 - ($statData2[3] * 100 / $cpuTime);
                 }
             }
         }
 
-        if (is_null($memoryTotal) || is_null($memoryFree)) {
-            return null;
-        } else {
-            if ($getPercentage) {
-                return (100 - ($memoryFree * 100 / $memoryTotal));
-            } else {
-                return array(
-                    "total" => $memoryTotal,
-                    "free" => $memoryFree,
-                );
-            }
-        }
+        return $load;
     }
 
-    function getNiceFileSize($bytes, $binaryPrefix=true) {
-        if ($binaryPrefix) {
-            $unit=array('B','KiB','MiB','GiB','TiB','PiB');
-            if ($bytes==0) return '0 ' . $unit[0];
-            return @round($bytes/pow(1024,($i=floor(log($bytes,1024)))),2) .' '. (isset($unit[$i]) ? $unit[$i] : 'B');
-        } else {
-            $unit=array('B','KB','MB','GB','TB','PB');
-            if ($bytes==0) return '0 ' . $unit[0];
-            return @round($bytes/pow(1000,($i=floor(log($bytes,1000)))),2) .' '. (isset($unit[$i]) ? $unit[$i] : 'B');
-        }
-    }
+    //----------------------------
 
-    // Memory usage: 4.55 GiB / 23.91 GiB (19.013557664178%)
-	function getMemoryServer() {
-		$memUsage = getServerMemoryUsage(false);
-		$getMemoryServer = sprintf("Memory usage: %s / %s (%s%%)",
-			getNiceFileSize($memUsage["total"] - $memUsage["free"]),
-			getNiceFileSize($memUsage["total"]),
-			getServerMemoryUsage(true)
-		);
-		return $getMemoryServer;
+	function getProcessorServer() {
+		$cpuLoad = getServerLoad();
+		if (is_null($cpuLoad)) {
+			return "CPU Usage: *Failed to measure*";
+			//return "CPU load not estimateable (maybe too old Windows or missing rights at Linux or Windows)";
+		}
+		else {
+			return $cpuLoad . "%";
+		}
 	}
 ?>
